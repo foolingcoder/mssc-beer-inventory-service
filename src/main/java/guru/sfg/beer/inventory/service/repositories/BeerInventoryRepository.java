f4e7b78cb37ef2c0a243d8411f2abf39b538ec4d
angump <angump@AGI-LAP-166.DCDMC.LOCAL>
2020-03-23 13:47:10 +0400

    Refactoring + Added AllocationService

--- src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
+++ src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
@@ -16,11 +16,14 @@
  */
 package guru.sfg.beer.inventory.service.repositories;
 
-import guru.sfg.beer.inventory.service.domain.BeerInventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+
+
 
 /**
  * Created by jt on 2019-01-26.
@@ -28,4 +31,6 @@ import java.util.UUID;
 public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
 
     List<BeerInventory> findAllByBeerId(UUID beerId);
+    
+    List<BeerInventory> findAllByUpc(String upc);
 }
